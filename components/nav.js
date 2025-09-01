72e2b30b5376361c6f2bd259f21f2611c4c169d5
Deax40 <62710850+Deax40@users.noreply.github.com>
2025-09-01 15:54:48 +0200

    refactor: remove certifications page

--- components/nav.js
+++ components/nav.js
@@ -11,20 +11,12 @@ export default function Nav({ active }) {
         <Link href="/care" className={`btn ${active === 'care' ? 'btn-primary' : ''}`}>CARE</Link>
         <Link href="/commun" className={`btn ${active === 'commun' ? 'btn-primary' : ''}`}>COMMUN</Link>
         {session?.user?.role === 'ADMIN' && (
-          <>
-            <Link
-              href="/admin/panel"
-              className={`btn ${active === 'admin' ? 'btn-primary' : ''}`}
-            >
-              ADMIN
-            </Link>
-            <Link
-              href="/admin/certifications"
-              className={`btn ${active === 'certifications' ? 'btn-primary' : ''}`}
-            >
-              CERTIFICATIONS
-            </Link>
-          </>
+          <Link
+            href="/admin/panel"
+            className={`btn ${active === 'admin' ? 'btn-primary' : ''}`}
+          >
+            ADMIN
+          </Link>
         )}
       </div>
       <button className="btn w-full sm:w-auto" onClick={() => signOut({ callbackUrl: '/' })}>Se déconnecter</button>
