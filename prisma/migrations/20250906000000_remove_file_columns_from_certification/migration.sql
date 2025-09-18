7c4d793b6760fcdedc97dec3260e6e1e8e5f7ff1
Deax40 <62710850+Deax40@users.noreply.github.com>
2025-09-18 18:31:43 +0200

    Delete prisma directory

--- prisma/migrations/20250906000000_remove_file_columns_from_certification/migration.sql
+++
@@ -1,4 +0,0 @@
--- Drop file and expiration columns from Certification
-ALTER TABLE "Certification" DROP COLUMN IF EXISTS "file";
-ALTER TABLE "Certification" DROP COLUMN IF EXISTS "fileType";
-ALTER TABLE "Certification" DROP COLUMN IF EXISTS "expiresAt";
